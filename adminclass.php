8822f24233ec13fe9bad714338a49fc43ea908c4
benraj89 <78837264+benraj89@users.noreply.github.com>
2022-07-20 13:40:44 +0530

    Create adminclass.php

---
+++ adminclass.php
@@ -0,0 +1,176 @@
+ <?php
+#this is Login form page , if user is already logged in then we will not allow user to access this page by executing isset($_SESSION["uid"])
+#if below statment return true then we will send user to their profile.php page
+if (isset($_SESSION["uid"])) {
+	header("location:profile.php");
+}
+//in action.php page if user click on "ready to checkout" button that time we will pass data in a form from action.php page
+if (isset($_POST["login_user_with_product"])) {
+	//this is product list array
+	$product_list = $_POST["product_id"];
+	//here we are converting array into json format because array cannot be store in cookie
+	$json_e = json_encode($product_list);
+	//here we are creating cookie and name of cookie is product_list
+	setcookie("product_list",$json_e,strtotime("+1 day"),"/","","",TRUE);
+
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Khan Store</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+<body>
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<a href="#" class="navbar-brand">Khan Store</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+				<li><a href="index.php"><span class="glyphicon glyphicon-modal-window"></span>Product</a></li>
+			</ul>
+		</div>
+	</div>
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8" id="signup_msg">
+				<!--Alert from signup form-->
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<div class="panel panel-primary">
+					<div class="panel-heading">Customer Login Form</div>
+					<div class="panel-body">
+						<!--User Login Form-->
+						<form onsubmit="return false" id="login">
+							<label for="email">Email</label>
+							<input type="email" class="form-control" name="email" id="email" required/>
+							<label for="email">Password</label>
+							<input type="password" class="form-control" name="password" id="password" required/>
+							<p><br/></p>
+							<a href="#" style="color:#333; list-style:none;">Forgotten Password</a><input type="submit" class="btn btn-success" style="float:right;" Value="Login">
+							<!--If user dont have an account then he/she will click on create account button-->
+							<div><a href="customer_registration.php?register=1">Create a new account?</a></div>						
+						</form>
+				</div>
+				<div class="panel-footer"><div id="e_msg"></div></div>
+			</div>
+		</div>
+		<div class="col-md-4"></div>
+	</div>
+</body>
+</html>
+ <?php
+session_start();
+$ip_add = getenv("REMOTE_ADDR");
+include "db.php";
+if(isset($_POST["category"])){
+	$category_query = "SELECT * FROM categories";
+	$run_query = mysqli_query($con,$category_query) or die(mysqli_error($con));
+	echo "
+		<div class='nav nav-pills nav-stacked'>
+			<li class='active'><a href='#'><h4>Categories</h4></a></li>
+	";
+	if(mysqli_num_rows($run_query) > 0){
+		while($row = mysqli_fetch_array($run_query)){
+			$cid = $row["cat_id"];
+			$cat_name = $row["cat_title"];
+			echo "
+					<li><a href='#' class='category' cid='$cid'>$cat_name</a></li>
+			";
+		}
+		echo "</div>";
+	}
+}
+if(isset($_POST["brand"])){
+	$brand_query = "SELECT * FROM brands";
+	$run_query = mysqli_query($con,$brand_query);
+	echo "
+		<div class='nav nav-pills nav-stacked'>
+	…
+[12:39 PM, 7/20/2022] IED: <?php
+
+require "config/constants.php";
+
+$servername = HOST;
+$username = USER;
+$password = PASSWORD;
+$db = DATABASE_NAME;
+
+// Create connection
+$con = mysqli_connect($servername, $username, $password,$db);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+?>
+[12:39 PM, 7/20/2022] IED: <?php
+
+/**
+ * 
+ */
+class Database
+{
+	
+	private $con;
+	public function connect(){
+		$this->con = new Mysqli("localhost", "root", "", "khanstore");
+		return $this->con;
+	}
+}
+
+?>
+ <?php
+
+session_start();
+if(!isset($_SESSION["uid"])){
+	header("location:index.php");
+}
+
+if (isset($_GET["st"])) {
+
+	# code...
+	$trx_id = $_GET["tx"];
+		$p_st = $_GET["st"];
+		$amt = $_GET["amt"];
+		$cc = $_GET["cc"];
+		$cm_user_id = $_GET["cm"];
+		$c_amt = $_COOKIE["ta"];
+	if ($p_st == "Completed") {
+
+		
+
+		include_once("db.php");
+		$sql = "SELECT p_id,qty FROM cart WHERE user_id = '$cm_user_id'";
+		$query = mysqli_query($con,$sql);
+		if (mysqli_num_rows($query) > 0) {
+			# code...
+			while ($row=mysqli_fetch_array($query)) {
+			$product_id[] = $row["p_id"];
+			$qty[] = $row["qty"];
+			}
+
+			for ($i=0; $i < count($product_id); $i++) { 
+				$sql = "INSERT INTO orders (user_id,product_id,qty,trx_id,p_status) VALUES ('$cm_user_id','".$product_id[$i]."','".$qty[$i]."','$trx_id','$p_st')";
+				mysqli_query($con,$sql);
+			}
